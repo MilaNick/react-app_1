9f565def07e28c496ca9709d038902004d488608
Mila Zolotova <l.zolotova@yahoo.com>
2022-05-11 19:33:28 +0300

    added filters & search

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,10 +13,12 @@ class App extends Component {
     super(props);
     this.state = {
       data: [
-        {name: 'Victor I.', salary: 800, increase: true, rise: false, id: 1},
-        {name: 'Alex V.', salary: 1000, increase: false, rise: false, id: 2},
+        {name: 'Victor I.', salary: 800, increase: false, rise: true, id: 1},
+        {name: 'Alex V.', salary: 1000, increase: true, rise: false, id: 2},
         {name: 'Carl S.', salary: 5000, increase: false, rise: false, id: 3},
       ],
+      term: '',
+      filter: 'all',
     }
     this.maxId = 4;
   }
@@ -54,23 +56,51 @@ class App extends Component {
     }))
   }
 
+  seachEmp = (items, term) => {
+    if(term.length === 0) {
+      return items;
+    }
+    return items.filter(item => {
+      return item.name.toLowerCase().indexOf(term.toLowerCase()) > -1
+    })
+  }
+
+  onUpdateSearch = (term) => {
+    this.setState({term});
+  }
+
+  filterPost = (items, filter) => {
+    switch (filter) {
+      case 'rise':
+        return items.filter(item => item.rise);
+      case 'moreThen1000':
+        return items.filter(item => item.salary > 1000);
+      default:
+        return items;
+    }
+  }
+
+  onFilterSelect = (filter) => {
+    this.setState({filter});
+  }
+
   render() {
+    const {data, term, filter} = this.state;
     const employees = this.state.data.length;
     const increased = this.state.data.filter(item => item.increase).length;
-    const { data } = this.state;
+    const visibleData = this.filterPost(this.seachEmp(data, term), filter);
+
     return (
       <div className='app'>
         <Info employees={employees} increased={increased}/>
+
         <div className='search'>
-          <Search/>
-          <Filter/>
+          <Search onUpdateSearch={this.onUpdateSearch}/>
+          <Filter filter={filter} onFilterSelect={this.onFilterSelect} />
         </div>
-        <EmployeesList
-          data={data}
-          onDelete={this.deleteItem}
-          onToggleProp={this.onToggleProp}/>
-        <EmployeesAddForm
-          onAdd={this.addItem}/>
+
+        <EmployeesList data={visibleData} onDelete={this.deleteItem} onToggleProp={this.onToggleProp}/>
+        <EmployeesAddForm onAdd={this.addItem}/>
       </div>
     );
   }
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,12 +1,33 @@
+import {Component} from 'react';
+
 import './search.css';
 
-const Search = () => {
-  return (
-    <input
-      type='text'
-      className='form-control search-input'
-      placeholder='Найти сотрудника' />
-  )
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+    }
+  }
+
+  onUpdateSearch_ = (e) => {
+    const term = e.target.value;
+    this.setState({term})
+    this.props.onUpdateSearch(term)
+  }
+
+  render() {
+    return (
+      <input
+        type='text'
+        className='form-control search-input'
+        placeholder='Найти сотрудника'
+        value={this.state.term}
+        onChange={this.onUpdateSearch_ }
+      />
+    )
+  }
+
 }
 
 export default Search;
